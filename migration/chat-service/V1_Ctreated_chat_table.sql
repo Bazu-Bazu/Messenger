f480759c75b05f5161283f420e5debf2cfc7ee03
Bazu-Bazu <iv.dima1304@gmail.com>
2025-11-20 01:22:00 +0300

    Added new tables

---
+++ migration/chat-service/V1_Ctreated_chat_table.sql
@@ -0,0 +1,28 @@
+CREATE TABLE chat (
+                      id BIGSERIAL PRIMARY KEY,
+                      type VARCHAR(7) NOT NULL,
+                      name VARCHAR(40),
+                      description VARCHAR(500),
+                      avatar_url VARCHAR(255),
+                      created_by BIGINT,
+                      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE chat ADD CONSTRAINT chat_type_check
+    CHECK (type IN ('PRIVATE', 'GROUP', 'CHANNEL'));
+
+-- Создаем функцию для обновления updated_at
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- Создаем триггер для таблицы users
+CREATE TRIGGER update_users_updated_at
+    BEFORE UPDATE ON chat
+    FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
