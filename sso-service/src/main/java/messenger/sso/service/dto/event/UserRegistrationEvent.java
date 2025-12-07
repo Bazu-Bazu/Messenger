9c7c2fc1628327ab6bce67bab897c20a3cbfb0d9
BazuBazu <iv.dima1304@gmail.com>
2025-12-07 20:49:14 +0300

    Added the ability to change user's properties

--- sso-service/src/main/java/messenger/sso/service/dto/event/UserRegistrationEvent.java
+++ sso-service/src/main/java/messenger/sso/service/dto/event/UserRegistrationEvent.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 @Builder
 public record UserEvent(
         Long id,
-        String userName,
+        String username,
         String phone,
         String email,
         String password
