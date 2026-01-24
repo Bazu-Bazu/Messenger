9e5129084ad95aa0ab8a1f801356e1f96642ace0
BazuBazu <iv.dima1304@gmail.com>
2026-01-24 21:50:09 +0300

    Refactoring

--- message-service/src/main/java/messenger/message/service/controller/api/MessageController.java
+++ message-service/src/main/java/messenger/message/service/controller/api/MessageController.java
@@ -2,7 +2,6 @@ package messenger.message.service.controller.api;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import dto.request.EditMessageRequest;
 import dto.request.GetMessagesRequest;
 import dto.request.MarkMessageAsReadRequest;
@@ -17,7 +16,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/message")
 @RequiredArgsConstructor
-@Log4j2
 public class MessageController {
 
     private final MessageService messageService;
@@ -27,12 +25,7 @@ public class MessageController {
             @RequestParam("senderId") @Valid Long senderId,
             @RequestBody @Valid SendMessageRequest request
     ) {
-        log.info("Sending message attempt by user {} and {} chat {}",
-                senderId, request.chatType(), request.chatId());
-
         MessageResponse response = messageService.sendMessage(request, senderId);
-
-        log.info("User {} sent message to {} chat {}", senderId, request.chatType(), request.chatId());
 
         return ResponseEntity.status(201).body(response);
     }
@@ -42,13 +35,7 @@ public class MessageController {
             @RequestParam("getterId") @Valid Long getterId,
             @RequestBody @Valid GetMessagesRequest request
     ) {
-        log.info("Getting messages attempt by user {} and {} chat {}",
-                getterId, request.chatType(), request.chatId());
-
         List<MessageResponse> responses = messageService.getChatMessages(getterId, request);
-
-        log.info("User {} got messages from {} chat {} successfully",
-                getterId, request.chatType(), request.chatId());
 
         return ResponseEntity.status(200).body(responses);
     }
@@ -58,13 +45,7 @@ public class MessageController {
             @RequestParam("editorId") @Valid Long editorId,
             @RequestBody @Valid EditMessageRequest request
     ) {
-        log.info("Editing message attempt by user {} and message {} in {} chat {}",
-                editorId, request.messageId(), request.chatType(), request.chatId());
-
         MessageResponse response = messageService.editMessage(request, editorId);
-
-        log.info("User {} edited message {} in {} chat {} successfully",
-                editorId, request.messageId(), request.chatType(), request.chatId());
 
         return ResponseEntity.status(200).body(response);
     }
@@ -74,11 +55,7 @@ public class MessageController {
             @RequestParam("readerId") @Valid Long readerId,
             @RequestBody @Valid MarkMessageAsReadRequest request
     ) {
-        log.info("Mark message {} as read attempt bu user {}", request.messageId(), readerId);
-
         MessageResponse response = messageService.markMessageAsRead(readerId, request);
-
-        log.info("Message {} marked as read by user {} successfully", request.messageId(), readerId);
 
         return ResponseEntity.status(200).body(response);
     }
