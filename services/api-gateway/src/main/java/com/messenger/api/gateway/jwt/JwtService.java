dc07a1e8b70ec4f24254bfdd29cb368c6bc2fdf0
BazuBazu <iv.dima1304@gmail.com>
2026-01-31 17:18:49 +0300

    Added jwt filter and service and connected gateway with sso

---
+++ services/api-gateway/src/main/java/com/messenger/api/gateway/jwt/JwtService.java
@@ -0,0 +1,76 @@
+package com.messenger.api.gateway.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.List;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret-key}")
+    private String jwtSecret;
+
+    public boolean isTokenValid(String token) {
+        try {
+            extractAllClaims(token);
+            return !isTokenExpired(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String extractUserId(String token) {
+        return extractClaim(token, claims -> claims.get("user_id", String.class));
+    }
+
+    public List<String> extractRoles(String token) {
+        return extractClaim(token, claims ->
+                claims.get("authorities", List.class)
+        );
+    }
+
+    public boolean isRefreshToken(String token) {
+        return "REFRESH".equals(
+                extractClaim(token, c -> c.get("token_type", String.class))
+        );
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
